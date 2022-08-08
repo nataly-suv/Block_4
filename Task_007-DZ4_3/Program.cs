44f894ab5db78e003b51a8c5bb7f7a64c08924ba
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-08 23:22:47 +0300

    ДЗ 4. Задача 3. Массив из 8 элементов

---
+++ Task_007-DZ4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа, которая задает массив из 8 элементов и выводит их на экран
+
+// метод заполнения массива 
+void FillArray(int[] array)
+{
+    System.Random numbers = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = numbers.Next(10, 101);
+    }
+}
+
+// Метод печати массива
+void Print (int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+int[] massiv = new int[8];
+FillArray(massiv);
+Print(massiv);
+
+ 
