6ed9a52d486f73bd14b07341edb3b5f0c8939f72
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-08 23:21:10 +0300

    ДЗ 4. Задача 2. Сумма цифр в числе

---
+++ Task_006-DZ4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Программа, кототрая принимает число, и
+   выдает сумму цифр в числе 
+*/
+
+// метод сложения цифр 
+int Sum(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        sum = sum + x % 10;
+        x = x / 10;
+    }
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+DateTime time = DateTime.Now;
+Console.WriteLine("Сумма цифр в числе равна: " + Sum(num));
+Console.WriteLine(DateTime.Now - time);
+
+
+
