3c8bc626355068f54203f6b0be2d31723bf599fa
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-08 23:18:24 +0300

    Семинар 4. Задача 4. Массив из 8 элементов

---
+++ Task_004-Sem4_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Заполнить массив из 8 элементов, случайными элементами 0 и 1
+
+System.Random numberSintezator = new Random();
+
+void Variant1()
+{
+    int i = 0;
+    while (i<7)
+    {
+        Console.Write($"{numberSintezator.Next(0,2)}, ");
+        i++;
+    }
+}
+
+Console.Write("[");
+Variant1();
+Console.Write($"{numberSintezator.Next(0,2)}");
+Console.Write("]");
