660732e5bcde13f4933bc0d94b3f58429434a8a6
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-08 23:16:58 +0300

    Семинар 4. Задача 3. Произведение от 1 до N

---
+++ Task_003-Sem4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// произведение от 1 до N 
+
+Console.Write ("Введите число: ");
+int inNumber = Convert.ToInt32 (Console.ReadLine());
+
+void Multi(int x)
+{
+    long y = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        y *=i;
+    }
+    Console.WriteLine("1. Произведенеи равно: " + y);
+}
+
+
+Multi(inNumber); 
+
+// // через рекурсию 
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+ 
+//     return n * Factorial(n - 1);
+// }
+// Console.WriteLine (Factorial(inNumber));
