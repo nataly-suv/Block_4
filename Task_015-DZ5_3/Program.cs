78f8e08169394fa7a08d5f34c6f0243869ad9af4
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-12 18:02:10 +0300

    ДЗ-5. Задача 3. Массив вещественных чисел

---
+++ Task_015-DZ5_3/Program.cs
@@ -0,0 +1,50 @@
+﻿/* =====================================================
+Задайте массив вещественных чисел. Найдите разницу между 
+максимальным и минимальным элементов массива.
+====================================================== */
+
+int length = 10;
+
+// массив вещественных чисел
+double[] FillArrayDouble()
+{
+    System.Random numberSintezator = new Random();
+    double[] outArray = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        outArray[i] = numberSintezator.NextDouble();
+    }
+    return outArray;
+}
+
+// печать массива 
+void PrintArrayDouble(double[] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        array[i]=Math.Round(array[i] * 100, 3);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// метод поиска разницы между мах и мин
+void DifMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < (array.Length - 1); i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+    }
+    double result = max-min;
+    Console.WriteLine("Максимальное число в массвие: " + max);
+    Console.WriteLine("Минимальное число в массвие: " + min);
+    Console.WriteLine($"Разница между мах и мин: {result}");
+}
+
+
+double[] bufArray = FillArrayDouble();
+PrintArrayDouble(bufArray);
+DifMaxMin(bufArray);
