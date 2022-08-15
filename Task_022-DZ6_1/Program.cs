2d7f3172540d93ea8024d6b0f30bd8807e75c0fa
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-15 16:10:25 +0300

    ДЗ 6. Задача 1. Подсчет + чисел

---
+++ Task_022-DZ6_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* ==================================================
+Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько  чисел больше 0 ввёл пользователь.
+=====================================================*/
+
+Console.WriteLine("Введите ряд чисел: ");
+string? inputString = Console.ReadLine();
+
+inputString = inputString.Replace(" ", ""); // удадида пробелы 
+
+int countComma = 0;
+int countMinus = 0;
+int countNull = 0;
+
+if (inputString.Length != 0)
+{
+    for (var i = 0; i < inputString.Length; i++)
+    {
+        if (inputString[i] == ',') countComma = countComma + 1;
+        if (inputString[i] == '-') countMinus = countMinus + 1;
+        if (inputString[i] == '0') countNull = countNull + 1;
+    }
+    int answer = countComma + 1 - countMinus - countNull;
+    Console.WriteLine($"Количество положительных чисел: {answer}");
+}
+else Console.WriteLine("Вы ввели пустую строку");
+ 
+
+
