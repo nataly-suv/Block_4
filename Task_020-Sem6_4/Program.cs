d014c2680e232c80508128a740959b199b4aaf90
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-15 16:06:46 +0300

    Семинар 6. Задача 4. Ряд Фибоначи

---
+++ Task_020-Sem6_4/Program.cs
@@ -0,0 +1,45 @@
+﻿/* =================================================
+Не используя рекурсию, выведите первые N чисел
+Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Fn=F(n-1)+ F(n-2)
+================================================= */
+
+Console.Write("Введите кол-во чисел в ряде Фибоначи: ");
+int inNum = Convert.ToInt32(Console.ReadLine());
+
+int[] RowFibonachi(int num)
+{
+    int[] array = new int[num];
+    if (array.Length == 1) array[0] = 0;
+    else if (array.Length == 2)
+    {
+        array[0] = 0;
+        array[1] = 1;
+    }
+    else
+    {
+        for (int i = 2; i < array.Length; i++)
+        {
+            array[0] = 0;
+            array[1] = 1;
+            array[i] = array[i-1] + array[i-2];
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] rowFib = RowFibonachi(inNum);
+PrintArray(rowFib);
+
+
