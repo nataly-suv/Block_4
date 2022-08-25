debafd24cfb4374544ef03cf827b56ace9c5306e
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-25 18:12:23 +0300

    ДЗ 9. Задача 1. Рекурсия. Ряд от N до 1

---
+++ Task_044-DZ9_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* =================================================================
+Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа от N до 1
+==================================================================*/
+int count = 1;
+
+// метод считывания данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+// метод вывода ряда от N до 1 (рекурсия)
+void ReverseRow(int count, int number)
+{
+    if (count > number) return; // условие остановки рекурсии 
+    else
+    {        
+        // печатает ход рекурсии 
+        Console.WriteLine(count);
+        // метод вызывает сам себя, но с  увеличением счетчика
+        ReverseRow(count + 1, number);
+        Console.Write( count + " ");
+    }
+}
+
+
+int inputNumber = ReadData("Задайте число: ");
+ReverseRow(count, inputNumber);
+
