78a90681e5c83418d0e1763b9655de7707d63107
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-08-25 13:57:11 +0300

    Семинар 9. Задача 2. Рекурсия. Вывод от M до N

---
+++ Task_041-Sem9_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/* =========================================================
+Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+===========================================================*/
+
+
+
+// метод считывания данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// метод вывода натуральных числе от m до n (рукурсия) 
+void NaturalNamberPrinter(int m, int n)
+{
+    // m-1 - чтобы напечаталась последняя цифра 
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNamberPrinter(m + 1, n);
+}
+
+
+
+int numberM = ReadData("Задайте число M: ");
+int numberN = ReadData("Задайте число N: ");
+
+if (numberM < numberN)
+    NaturalNamberPrinter(numberM, numberN);
+else
+    NaturalNamberPrinter(numberN, numberM);
